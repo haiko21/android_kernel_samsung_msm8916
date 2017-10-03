0e13952f5e3a390e5391d15f83a3611cd8f82c6d
muhviehstah <t.sarawinski@gmail.com>
2017-10-03 06:46:23 +0200

    fixes

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -29,19 +29,6 @@
 #define ULLONG_MAX	(~0ULL)
 #define SIZE_MAX	(~(size_t)0)
 
-#define U8_MAX         ((u8)~0U)
-#define S8_MAX         ((s8)(U8_MAX>>1))
-#define S8_MIN         ((s8)(-S8_MAX - 1))
-#define U16_MAX        ((u16)~0U)
-#define S16_MAX        ((s16)(U16_MAX>>1))
-#define S16_MIN        ((s16)(-S16_MAX - 1))
-#define U32_MAX        ((u32)~0U)
-#define S32_MAX        ((s32)(U32_MAX>>1))
-#define S32_MIN        ((s32)(-S32_MAX - 1))
-#define U64_MAX        ((u64)~0ULL)
-#define S64_MAX        ((s64)(U64_MAX>>1))
-#define S64_MIN        ((s64)(-S64_MAX - 1))
-
 #define U8_MAX		((u8)~0U)
 #define S8_MAX		((s8)(U8_MAX>>1))
 #define S8_MIN		((s8)(-S8_MAX - 1))
@@ -219,10 +206,13 @@ extern int _cond_resched(void);
 		(__x < 0) ? -__x : __x;		\
 	})
 
-#if defined(CONFIG_PROVE_LOCKING) || defined(CONFIG_DEBUG_ATOMIC_SLEEP)
+#ifdef CONFIG_PROVE_LOCKING
 void might_fault(void);
 #else
-static inline void might_fault(void) { }
+static inline void might_fault(void)
+{
+	might_sleep();
+}
 #endif
 
 extern struct atomic_notifier_head panic_notifier_list;
@@ -574,7 +564,7 @@ do {									\
 	__trace_printk_check_format(fmt, ##args);			\
 									\
 	if (__builtin_constant_p(fmt))					\
-		__trace_printk(_THIS_IP_, trace_printk_fmt, ##args);	\
+		__trace_bprintk(_THIS_IP_, trace_printk_fmt, ##args);	\
 	else								\
 		__trace_printk(_THIS_IP_, fmt, ##args);			\
 } while (0)
@@ -814,8 +804,5 @@ static inline void ftrace_dump(enum ftrace_dump_mode oops_dump_mode) { }
 #ifdef CONFIG_FTRACE_MCOUNT_RECORD
 # define REBUILD_DUE_TO_FTRACE_MCOUNT_RECORD
 #endif
-
-/* To identify board information in panic logs, set this */
-extern char *mach_panic_string;
 
 #endif
